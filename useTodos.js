206ee8cf6eaea50b4d3cd84591a8b44ba5a66b9a
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2023-02-24 15:43:22 -0500

    primer commit

---
+++ useTodos.js
@@ -0,0 +1,43 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos) || []);
+  }, [todos]);
+
+  const handleAddTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    dispatchTodo(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove Todo",
+      payload: id,
+    };
+
+    dispatchTodo(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    };
+
+    dispatchTodo(action);
+  };
+
+  return { todos, handleAddTodo, handleDeleteTodo, handleToggleTodo,todosCount: todos.length,
+    pendingTodos:todos.filter(todo => !todo.done).length };
+};
